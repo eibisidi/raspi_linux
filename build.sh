c1bcf9750db64ae0384ef64cccf937e29002a7b0
eibisidi <eibisidi@qq.com>
2024-10-11 19:57:29 -0700

    change build shell scripte

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+BEAR=
 
+#bear will generate complete_commands.json. clangd complaints about unknown flag -lp64
+#remove lp64 with following command
+#vim complete_commands.json
+#:%s/"mabi=lp64"/""/g
+if [ "$1" == "BEAR" ];
+then
+	echo "Build with bear"
+	BEAR=bear
+else
+	echo "Normal Build"
+fi
 
-
-make V=0 -j12 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+${BEAR} make V=0 -j12 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
 
 
